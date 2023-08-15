82728170838386f85b2db64692b4898524c727ec
Kevin Dev <kevin.developer71@gmail.com>
2023-08-15 16:27:54 -0600

    chore(eslint): config eslint and prettier rules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,19 @@
-import '@/styles/globals.css'
+import { Inter } from "next/font/google"
+
+import "@/styles/globals.css"
+
+import { cn } from "@/lib/utils"
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang="en" className={cn("bg-white text-slate-900 antialiased light", inter.className)}>
       <body>{children}</body>
     </html>
   )
