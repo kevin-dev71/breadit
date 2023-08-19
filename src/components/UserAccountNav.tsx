0349ced6e4f606a5ee1b65dee8bbde98619c97b6
Kevin Dev <kevin.developer71@gmail.com>
2023-08-19 14:36:47 -0600

    feat: signout

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,8 +1,18 @@
+"use client"
+
+import Link from "next/link"
 import type { User } from "next-auth"
+import { signOut } from "next-auth/react"
 import type { FC } from "react"
 
-import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import UserAvatar from "@/components/UserAvatar"
+import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu"
 
 interface UserAccountNavProps {
   user: Pick<User, "name" | "email" | "image">
@@ -20,6 +30,42 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
           }}
         />
       </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="bg-white" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && <p className="w-[200px] truncate text-sm text-zinc-700">{user.email}</p>}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href="/">Feed</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/r/create">Create community</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/settings">Settings</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem
+          onSelect={(event) => {
+            event.preventDefault()
+            signOut({
+              callbackUrl: `${window.location.origin}/sign-in`,
+            })
+          }}
+          className="cursor-pointer"
+        >
+          Sign out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   )
 }
