b81b430c46a25b896c48419d4e62e2a6e6f1eb81
Kevin Dev <kevin.developer71@gmail.com>
2023-08-21 21:23:02 -0600

    feat: error handling and api route for create community

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -2,7 +2,10 @@
 
 import { useState } from "react"
 import { useRouter } from "next/navigation"
-import axios from "axios"
+import axios, { AxiosError } from "axios"
+
+import { usecustomToast } from "@/hooks/use-custom-toast"
+import { toast } from "@/hooks/use-toast"
 
 import { Button } from "@/components/ui/Button"
 import { Input } from "@/components/ui/input"
@@ -12,6 +15,7 @@ import { useMutation } from "@tanstack/react-query"
 const Page = () => {
   const [input, setInput] = useState("")
   const router = useRouter()
+  const { loginToast } = usecustomToast()
 
   const { mutate: createCommunity, isLoading } = useMutation({
     mutationFn: async () => {
@@ -21,6 +25,28 @@ const Page = () => {
 
       const { data } = await axios.post("/api/subreddit", payload)
       return data as string
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409) {
+          return toast({
+            title: "Subreddit already exists.",
+            description: "Please try another name.",
+            variant: "destructive",
+          })
+        }
+        if (err.response?.status === 422) {
+          return toast({
+            title: "Invalid Subreddit name.",
+            description: "Please choose a name between 3 and 21 characters.",
+            variant: "destructive",
+          })
+        }
+
+        if (err.response?.status === 401) {
+          return loginToast()
+        }
+      }
     },
   })
 
